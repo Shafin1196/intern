58059497c81466bfa75a6ef7778c33368418046a
shafin1196 <shafin23105101196@diu.edu.bd>
2025-09-11 13:52:14 +0600

    overlay

--- lib/homeWidgets/parcel_column.dart
+++ lib/homeWidgets/parcel_column.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-Widget buttonTile(String title,String subtitle,Icons icon,Function()? onTap){
+Widget buttonTile({required String title, required String subtitle, required IconData icon, required VoidCallback onTap}){
   return InkWell(
-          onTap: () {
-            //handle later
-          },
+          onTap: onTap,
           child: Card(
             margin: EdgeInsets.only(top: 10, left: 15, right: 15, bottom: 5),
             shape: RoundedRectangleBorder(
@@ -18,10 +16,10 @@ Widget buttonTile(String title,String subtitle,Icons icon,Function()? onTap){
               child: ListTile(
                 leading: CircleAvatar(
                   backgroundColor: Colors.grey.shade300,
-                  child: Icon(Icons.restaurant_menu_outlined,color: Colors.green,),
+                  child: Icon(icon,color: Colors.green,),
                 ),
-                title: Text('Homemade food ',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 14,color: Colors.black),),
-                subtitle: Text('Home-cooked meals',style: TextStyle(fontSize: 14,color: Colors.grey.shade500),),
+                title: Text(title,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 14,color: Colors.black),),
+                subtitle: Text(subtitle,style: TextStyle(fontSize: 14,color: Colors.grey.shade500),),
               ),
             ),
           ),
@@ -33,206 +31,32 @@ class ParcelColumn extends ConsumerWidget{
     return Column(
       mainAxisAlignment: MainAxisAlignment.start,
       children: [
-        InkWell(
-          onTap: () {
-            //handle later
-          },
-          child: Card(
-            margin: EdgeInsets.only(top: 10, left: 15, right: 15, bottom: 5),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-              side: BorderSide(color: Colors.grey.shade300,
-              width: 1,
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 8, right: 8,top:6,bottom:6),
-              child: ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.grey.shade300,
-                  child: Icon(Icons.restaurant_menu_outlined,color: Colors.green,),
-                ),
-                title: Text('Homemade food ',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 14,color: Colors.black),),
-                subtitle: Text('Home-cooked meals',style: TextStyle(fontSize: 14,color: Colors.grey.shade500),),
-              ),
-            ),
-          ),
-        ),
-        InkWell(
-          onTap: () {
-            //handle later
-          },
-          child: Card(
-            margin: EdgeInsets.only(top: 10, left: 15, right: 15, bottom: 5),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-              side: BorderSide(color: Colors.grey.shade300,
-              width: 1,
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 8, right: 8,top:6,bottom:6),
-              child: ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.grey.shade300,
-                  child: Icon(Icons.restaurant_menu_outlined,color: Colors.green,),
-                ),
-                title: Text('Homemade food ',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 14,color: Colors.black),),
-                subtitle: Text('Home-cooked meals',style: TextStyle(fontSize: 14,color: Colors.grey.shade500),),
-              ),
-            ),
-          ),
-        ),
-        InkWell(
-          onTap: () {
-            //handle later
-          },
-          child: Card(
-            margin: EdgeInsets.only(top: 10, left: 15, right: 15, bottom: 5),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-              side: BorderSide(color: Colors.grey.shade300,
-              width: 1,
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 8, right: 8,top:6,bottom:6),
-              child: ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.grey.shade300,
-                  child: Icon(Icons.restaurant_menu_outlined,color: Colors.green,),
-                ),
-                title: Text('Homemade food ',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 14,color: Colors.black),),
-                subtitle: Text('Home-cooked meals',style: TextStyle(fontSize: 14,color: Colors.grey.shade500),),
-              ),
-            ),
-          ),
-        ),
-        InkWell(
-          onTap: () {
-            //handle later
-          },
-          child: Card(
-            margin: EdgeInsets.only(top: 10, left: 15, right: 15, bottom: 5),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-              side: BorderSide(color: Colors.grey.shade300,
-              width: 1,
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 8, right: 8,top:6,bottom:6),
-              child: ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.grey.shade300,
-                  child: Icon(Icons.restaurant_menu_outlined,color: Colors.green,),
-                ),
-                title: Text('Homemade food ',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 14,color: Colors.black),),
-                subtitle: Text('Home-cooked meals',style: TextStyle(fontSize: 14,color: Colors.grey.shade500),),
-              ),
-            ),
-          ),
-        ),
-        InkWell(
-          onTap: () {
-            //handle later
-          },
-          child: Card(
-            margin: EdgeInsets.only(top: 10, left: 15, right: 15, bottom: 5),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-              side: BorderSide(color: Colors.grey.shade300,
-              width: 1,
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 8, right: 8,top:6,bottom:6),
-              child: ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.grey.shade300,
-                  child: Icon(Icons.restaurant_menu_outlined,color: Colors.green,),
-                ),
-                title: Text('Homemade food ',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 14,color: Colors.black),),
-                subtitle: Text('Home-cooked meals',style: TextStyle(fontSize: 14,color: Colors.grey.shade500),),
-              ),
-            ),
-          ),
-        ),
-        InkWell(
-          onTap: () {
-            //handle later
-          },
-          child: Card(
-            margin: EdgeInsets.only(top: 10, left: 15, right: 15, bottom: 5),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-              side: BorderSide(color: Colors.grey.shade300,
-              width: 1,
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 8, right: 8,top:6,bottom:6),
-              child: ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.grey.shade300,
-                  child: Icon(Icons.restaurant_menu_outlined,color: Colors.green,),
-                ),
-                title: Text('Homemade food ',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 14,color: Colors.black),),
-                subtitle: Text('Home-cooked meals',style: TextStyle(fontSize: 14,color: Colors.grey.shade500),),
-              ),
-            ),
-          ),
-        ),
-        InkWell(
-          onTap: () {
-            //handle later
-          },
-          child: Card(
-            margin: EdgeInsets.only(top: 10, left: 15, right: 15, bottom: 5),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-              side: BorderSide(color: Colors.grey.shade300,
-              width: 1,
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 8, right: 8,top:6,bottom:6),
-              child: ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.grey.shade300,
-                  child: Icon(Icons.restaurant_menu_outlined,color: Colors.green,),
-                ),
-                title: Text('Homemade food ',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 14,color: Colors.black),),
-                subtitle: Text('Home-cooked meals',style: TextStyle(fontSize: 14,color: Colors.grey.shade500),),
-              ),
-            ),
-          ),
-        ),
-        InkWell(
-          onTap: () {
-            //handle later
-          },
-          child: Card(
-            margin: EdgeInsets.only(top: 10, left: 15, right: 15, bottom: 5),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-              side: BorderSide(color: Colors.grey.shade300,
-              width: 1,
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 8, right: 8,top:6,bottom:6),
-              child: ListTile(
-                leading: CircleAvatar(
-                  backgroundColor: Colors.grey.shade300,
-                  child: Icon(Icons.restaurant_menu_outlined,color: Colors.green,),
-                ),
-                title: Text('Homemade food ',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 14,color: Colors.black),),
-                subtitle: Text('Home-cooked meals',style: TextStyle(fontSize: 14,color: Colors.grey.shade500),),
-              ),
-            ),
-          ),
-        ),
+        buttonTile(title: 'Homemade food ', subtitle: 'Home-cooked meals', icon: Icons.restaurant_menu_outlined, onTap: () {
+          //assign later
+        }),
+        buttonTile(title: 'Clothes', subtitle: 'Folded or packed securely', icon: Icons.checkroom_outlined, onTap: () {
+          //assign later
+        }),
+        buttonTile(title: 'Documents ', subtitle: 'No passport or bank cheques', icon: Icons.document_scanner_outlined, onTap: () {
+          //assign later
+        }),
+        buttonTile(title: 'Gifts', subtitle: 'Flowers,cards,chocolates and others', icon: Icons.card_giftcard_outlined, onTap: () {
+          //assign later
+        }),
+        buttonTile(title: 'Cosmetics', subtitle: 'Makeup, skincare, or hygiene products.', icon: Icons.shopify_outlined, onTap: () {
+          //assign later
+        }),
+        buttonTile(title: 'Medicine', subtitle: 'Prescription or over the counter medicines', icon: Icons.medical_services_outlined, onTap: () {
+          //assign later
+        }),
+        buttonTile(title: 'Accessories ', subtitle: 'Watches, jewelry, bags, shoes etc,', icon: Icons.shopping_bag_outlined, onTap: () {
+          //assign later
+        }),
+        buttonTile(title: 'Electronics', subtitle: 'Stationery, small tools, or household items', icon: Icons.electrical_services_outlined, onTap: () {
+          //assign later
+        }),
+        
+        
       ],
     );
   }
